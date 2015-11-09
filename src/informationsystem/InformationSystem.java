3cbadf5b0fd79a70646dc2eece928474a17d4d9e
AlexseyKh <alexseykhanykin@gmail.com>
2015-11-09 12:50:26 +0400

    comm

--- src/informationsystem/InformationSystem.java
+++ src/informationsystem/InformationSystem.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -15,28 +15,4 @@ public class InformationSystem {
     public static void main(String args[]){
         new Frame1();
     }
-    
 }
-=======
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package informationsystem;
-
-/**
- *
- * @author Алексей
- */
-public class InformationSystem {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
-}
->>>>>>> refs/remotes/origin/master
